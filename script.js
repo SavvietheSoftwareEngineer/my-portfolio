772c968be3149faa62f3f8bfbb02efdc85a285f2
SavvietheSoftwareEngineer <151087435+SavvietheSoftwareEngineer@users.noreply.github.com>
2026-02-19 12:09:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        target.scrollIntoView({ behavior: 'smooth' });
+    });
+});
 
